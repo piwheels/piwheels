1ece3b9b858364df4911b6a131e50f01ebcf526a
Ben Nuttall <ben@bennuttall.com>
2017-04-24 01:48:32 +0100

    Add package web page generator

--- piwheels/tools.py
+++ piwheels/tools.py
@@ -38,3 +38,25 @@ def bash_dfh(path='/'):
     df = subprocess.Popen(['df', '-h', path], stdout=subprocess.PIPE)
     output = df.communicate()[0].split()
     return output[11].decode('UTF-8')
+
+def get_wheels_lis(db, package):
+    html = ''
+    wheels = db.get_package_wheels(package)
+    for wheel in wheels:
+        html += '<li><a href="{0}">{0}</a></li>'.format(wheel)
+    return html
+
+def get_package_output(db, package):
+    html = ''
+    builds = db.get_package_output(package)
+    for build_timestamp, status, output in builds:
+        status_msg = 'success' if status else 'fail'
+        html += '{} ({})\n{}\n\n'.format(build_timestamp, status_msg, output)
+    return html
+
+def get_install_instructions(package):
+    url = 'http://piwheels.bennuttall.com'
+    return """
+    <h2>Install</h2>
+    <pre>pip install {} -i {}</pre>
+    """.format(package, url)
