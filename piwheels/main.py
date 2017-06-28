7064b8dcb7283293d4f87a2cbdb28e13f8dfa82d
Ben Nuttall <ben@bennuttall.com>
2017-06-28 23:17:28 +0100

    Update main.py to new usage API

--- piwheels/main.py
+++ piwheels/main.py
@@ -1,23 +1,13 @@
-import better_exceptions
-
 from piwheels import PiWheelsBuilder
 from db import PiWheelsDatabase
-from auth import dbname, user, host, password
 
-db = PiWheelsDatabase(dbname, user, host, password)
+db = PiWheelsDatabase()
 
-
-def main(packages):
-    for package in packages:
-        if db.build_active():
-            builder = PiWheelsBuilder(package)
-            builder.build_wheel()
-            builder.log_build()
-        else:
-            print("The build is currently inactive")
-            break
-
-
-if __name__ == '__main__':
-    packages = db.get_unattempted_packages()
-    main(packages)
+for package, version in db.get_build_queue():
+    if db.build_active():
+        builder = PiWheelsBuilder(package, version)
+        builder.build_wheel()
+        builder.log_build()
+    else:
+        print("The build is currently inactive")
+        break
