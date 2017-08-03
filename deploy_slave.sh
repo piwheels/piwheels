81b6e1aa301670dbb13886259f8ae47198a51eac
Dave Jones <dave@waveform.org.uk>
2017-08-03 13:18:40 +0100

    Add a slave deployment script
    ... with a *huge* list of dependencies :)

---
+++ deploy_slave.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eu
+
+apt update
+apt -y upgrade
+apt -y install python3-zmq python-dev python3-dev zlib1g-dev libpng12-dev \
+    libffi-dev libxml2-dev libgmp-dev libhdf5-dev libldap2-dev libjpeg-dev \
+    libfreetype6-dev liblcms2-dev libzbar-dev libbz2-dev python-numpy \
+    python3-numpy python-scipy python3-scipy python-matplotlib \
+    python3-matplotlib python-pandas python3-pandas cython cython3 \
+    python-yaml python3-yaml python-lxml python3-lxml python-cffi \
+    python3-cffi python-bs4 python3-bs4 python-click \
+    python3-click python-sqlalchemy python3-sqlalchemy python-pil python3-pil \
+    python-pymongo python3-pymongo python-django python3-django python-flask \
+    python3-flask python-turbogears python-cherrypy python3-cherrypy3 \
+    python-qt4 python3-pyqt4 python-pyqt5 python3-pyqt5 python-pyside \
+    python3-pyside python-tornado python3-tornado python-pip python3-pip \
+    python-redis python3-redis python-dateutil python3-dateutil \
+    python-dnspython python3-dnspython python-sphinx python3-sphinx \
+    python-boto python3-boto python-gi python3-gi python-gi-cairo \
+    python3-gi-cairo python-cairocffi python3-cairocffi \
+    ipython ipython3 git tree byobu htop pkg-config
+pip3 install pip --upgrade
+pip3 install pypandoc
+pip3 install versioneer
+pip3 install kervi
+getent group piwheels || groupadd piwheels
+getent passwd piwheels || useradd -g piwheels -m piwheels
+passwd -d piwheels
+su -c "rm -fr piwheels" -l piwheels
+su -c "git clone https://github.com/bennuttall/piwheels" -l piwheels
