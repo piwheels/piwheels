ba9bfca7af4127d0fc0b5265c61818e006dc0494
Ben Nuttall <ben@bennuttall.com>
2018-08-31 10:16:54 +0100

    Tidy up deploy script

--- deploy_slave.sh
+++ deploy_slave.sh
@@ -41,27 +41,30 @@ apt -y install vim python3-zmq python-dev python3-dev zlib1g-dev $LIBPNG_DEV \
     libx11-6 libx11-dev xfonts-base xfonts-100dpi xfonts-75dpi \
     xfonts-cyrillic fluid-soundfont-gm musescore-soundfont-gm libsystemd-dev \
     $POSTGRES_SERVER_DEV
+    
 if [ $VERSION_ID -lt 9 ]; then
     pip3 install pip --upgrade
 fi
-pip3 install pypandoc
-pip3 install versioneer
-pip3 install kervi
+pip3 install pypandoc versioneer kervi
+
 getent passwd piwheels && userdel -fr piwheels
 getent group piwheels || groupadd piwheels
 getent passwd piwheels || useradd -g piwheels -m -s /bin/bash piwheels
 passwd -d piwheels
+
 if [ -d piwheels ]; then
-  cd piwheels
-  git pull
-  pip uninstall -y piwheels
+    cd piwheels
+    git pull
+    pip uninstall -y piwheels
 else
-  git clone https://github.com/bennuttall/piwheels
-  cd piwheels
+    git clone https://github.com/bennuttall/piwheels
+    cd piwheels
 fi
+
 cp piwheels-slave.service /etc/systemd/system/
 systemctl enable piwheels-slave.service
 pip3 install .
+
 if ! grep swapfile /etc/rc.local >/dev/null; then
     dd if=/dev/zero of=/swapfile bs=1M count=1024
     chmod 0600 /swapfile
