263ad9ec321598a5332a6f9a3b0da285cea9ba06
Ben Nuttall <ben@bennuttall.com>
2020-03-06 17:20:47 +0000

    Add libpcap0.8-dev to builders

--- deploy_slave.sh
+++ deploy_slave.sh
@@ -67,7 +67,7 @@ apt -y install vim ssh-import-id tree byobu htop pkg-config cmake time pandoc \
     xfonts-cyrillic fluid-soundfont-gm libsystemd-dev libusb-1.0-0-dev \
     libudev-dev libopus-dev libvpx-dev libc-bin libavdevice-dev \
     libadios-dev libavfilter-dev libavutil-dev libcec-dev lsb-release \
-    $PYBIND libsnappy-dev
+    $PYBIND libsnappy-dev libpcap0.8-dev
 
 pip3 install setuptools --upgrade
 pip3 install $PIP --upgrade
