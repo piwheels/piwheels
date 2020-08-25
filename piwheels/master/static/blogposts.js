f14222761d5fb2b120320752d359bd8e8f0329c5
Ben Nuttall <ben@bennuttall.com>
2020-08-25 23:51:34 +0100

    HTML decode blog post titles

--- piwheels/master/static/blogposts.js
+++ piwheels/master/static/blogposts.js
@@ -20,31 +20,38 @@ const formatDate = date => {
   return `${day} ${month} ${year}`;
 };
 
+const htmlDecode = input => {
+  const e = document.createElement('div');
+  e.innerHTML = input;
+  return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+};
+
 const showBlogPosts = (blogUrl, n) => {
   const ul = document.getElementById('latest-blog-posts');
   const blogJsonUrl = `${blogUrl}/wp-json/wp/v2/posts/?per_page=${n}`;
 
   fetch(blogJsonUrl)
-  .then(response => response.json())
-  .then(data => {
-    ul.innerHTML = '';
-    data.forEach(post => {
-      const li = document.createElement('li');
-      const a = document.createElement('a');
-      const postDate = new Date(post.date);
-      const postDateText = document.createTextNode(` (${formatDate(postDate)})`);
-      a.appendChild(document.createTextNode(post.title.rendered));
-      a.setAttribute('href', post.link);
-      li.appendChild(a);
-      li.appendChild(postDateText);
-      ul.appendChild(li);
+    .then(response => response.json())
+    .then(data => {
+      ul.innerHTML = '';
+      data.forEach(post => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const postDate = new Date(post.date);
+        const postDateText = document.createTextNode(` (${formatDate(postDate)})`);
+        const title = htmlDecode(post.title.rendered);
+        a.appendChild(document.createTextNode(title));
+        a.setAttribute('href', post.link);
+        li.appendChild(a);
+        li.appendChild(postDateText);
+        ul.appendChild(li);
+      });
+    })
+    .catch(error => {
+      const msg = 'Failed to load blog posts';
+      console.error(msg, error);
+      ul.innerHTML = `<li>${msg}</li>`;
     });
-  })
-  .catch(error => {
-    const msg = 'Failed to load blog posts';
-    console.error(msg, error);
-    ul.innerHTML = `<li>${msg}</li>`;
-  });
 };
 
 window.onload = () => {
