168305f666cfb38b46d972492cccd853c6f1efb0
Ben Nuttall <ben@bennuttall.com>
2022-02-13 16:45:26 +0000

    Add libsqlite3-dev and libsqlcipher-dev for sqlcipher3
    https://github.com/piwheels/packages/issues/246

--- deploy_slave.sh
+++ deploy_slave.sh
@@ -64,7 +64,7 @@ apt -y install vim ssh-import-id tree byobu htop pkg-config cmake time pandoc \
     libsnappy-dev libpcap0.8-dev swig libzmq5 portaudio19-dev libqpdf-dev \
     coinor-libipopt-dev libsrtp2-dev default-libmysqlclient-dev cargo golang \
     libgeos-dev $LIBGLES $LIBXLST $SOUNDFONT $POSTGRES_SERVER_DEV $TURBOGEARS \
-    $PYTHON2_PACKAGES $QMAKE libgphoto2-dev
+    $PYTHON2_PACKAGES $QMAKE libgphoto2-dev libsqlite3-dev libsqlcipher-dev
 
 apt purge python3-cryptography -y
 
