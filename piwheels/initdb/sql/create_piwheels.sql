ffe4faec4f005d0bb7b5de943e903bf852e2a1f3
Dave Jones <dave@waveform.org.uk>
2022-03-24 01:22:26 +0000

    Bump version for database update tests

--- piwheels/initdb/sql/create_piwheels.sql
+++ piwheels/initdb/sql/create_piwheels.sql
@@ -20,7 +20,7 @@ CREATE TABLE configuration (
     CONSTRAINT config_pk PRIMARY KEY (id)
 );
 
-INSERT INTO configuration(id, version) VALUES (1, '0.19');
+INSERT INTO configuration(id, version) VALUES (1, '0.20');
 GRANT SELECT ON configuration TO {username};
 
 -- packages
