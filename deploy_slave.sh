e21e96de37a46c46b74cbf37248b2009b425d4bb
MichaIng <micha@dietpi.com>
2023-05-15 21:10:43 +0200

    Revert config.txt edit
    The piwheels build systems runs via PXE TFTP boot and hence /boot/config.txt from the mounted rootfs is not used.
    
    The config.txt needs to be updated on the TFTP server instead, which then applies to all build system images.
    
    Signed-off-by: MichaIng <micha@dietpi.com>

--- deploy_slave.sh
+++ deploy_slave.sh
@@ -78,14 +78,6 @@ getent group piwheels || groupadd piwheels
 getent passwd piwheels || useradd -g piwheels -m -s /bin/bash piwheels
 passwd -d piwheels
 
-# Enforce 32-bit kernel as the 64-bit kernel is used by default (also on RPi OS 32-bit) since RPi Linux 6.1
-if [ -f /boot/config.txt ]; then
-    sed -i '/^[[:blank:]]*arm_64bit=/d' /boot/config.txt # remove setting first
-    sed -i '1iarm_64bit=0' /boot/config.txt # readd setting to top of file to assure it is effective on all RPi models
-else
-    echo 'arm_64bit=0' > /boot/config.txt
-fi
-
 curl -sSf 'https://sh.rustup.rs' | runuser -- - piwheels -s -- -y --profile minimal --default-host arm-unknown-linux-gnueabihf
 
 if [ -d piwheels ]; then
