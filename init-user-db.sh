b069e759cdec29be2ad55a3af4186eb0da2b3789
Ben Nuttall <ben@bennuttall.com>
2025-06-11 15:59:33 +0100

    Add test runner

--- init-user-db.sh
+++ init-user-db.sh
@@ -6,4 +6,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER piwheels WITH PASSWORD 'piwheels';
     CREATE DATABASE piwheels OWNER piwheels;
     GRANT ALL PRIVILEGES ON DATABASE piwheels TO piwheels;
+    CREATE USER piwsuper WITH SUPERUSER PASSWORD 'foobar';
+    CREATE DATABASE piwtest OWNER piwsuper;
+    GRANT ALL PRIVILEGES ON DATABASE piwtest TO piwheels;
 EOSQL
