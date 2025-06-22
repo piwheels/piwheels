061d69de1efb1ae977a3d8bc77ff0fd7cf0ff415
Ben Nuttall <ben@bennuttall.com>
2025-06-22 14:36:39 +0100

    Include package_names and preinstalled_apt_packages in minimal db restore

--- docker/db-restore-minimal.sql
+++ docker/db-restore-minimal.sql
@@ -1,6 +1,8 @@
-TRUNCATE configuration, build_abis, packages, versions RESTART IDENTITY CASCADE;
+TRUNCATE configuration, build_abis, packages, versions, package_names, preinstalled_apt_packages RESTART IDENTITY CASCADE;
 
 \COPY configuration(id, version, pypi_serial) FROM 'csv/configuration.csv' CSV;
 \COPY build_abis(abi_tag, skip) FROM 'csv/build_abis.csv' CSV;
 \COPY packages(package, skip, description) FROM 'csv/packages.csv' CSV;
 \COPY versions(package, version, released, skip, yanked) FROM 'csv/versions.csv' CSV;
+\COPY package_names(package, name, seen) FROM 'csv/package_names.csv' CSV;
+\COPY preinstalled_apt_packages(abi_tag, apt_package) FROM 'csv/preinstalled_apt_packages.csv' CSV;
